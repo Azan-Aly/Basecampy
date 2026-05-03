6e27f0f5f18e579bde96889295c792a0686cb270
Azan Aly <mr.n0bodypool@gmail.com>
2026-05-03 15:31:00 +0500

    Add models for project, task, subtask, note and projectmember

---
+++ backend/src/models/projectmember.models.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+import {AvailableUserRoles, UserRoleEnum} from "../utils/constants.js"
+import { User } from "./user.models.js";
+import { Project } from "./project.models.js";
+
+const projectMemberSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    project: {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: AvailableUserRoles,
+        default: UserRoleEnum.MEMBER
+    },
+
+}, {timestamps: true})
+
+
+export const ProjectMember = mongoose.model("ProjectMember", projectMemberSchema)
---
+++ backend/src/models/task.models.js
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+import {AvailableTaskStatues, TaskStatusEnum} from "../utils/constants.js"
+import { Project } from "./project.models.js";
+import { User } from "./user.models.js";
+
+const taskSchema = new Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    project: {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        required: true
+    },
+    assignedTo: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    assignedBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    status: {
+        type: String,
+        enum: AvailableTaskStatues,
+        default: TaskStatusEnum.TODO
+    },
+    attachment: {
+        type: [{
+            url: String,
+            mimetype: String,
+            size: Number
+        }],
+        default: []
+    }
+
+}, {timestamps: true})
+
+
+export const Task = mongoose.model("Task", taskSchema)
