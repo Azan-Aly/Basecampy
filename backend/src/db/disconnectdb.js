92f747ad8f71a119f3010187103f2ec1f6acca8e
Azan Aly <mr.n0bodypool@gmail.com>
2026-05-07 16:47:31 +0500

    Add smooth gracefull disconnection from db with error

---
+++ backend/src/db/disconnectdb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const disconnectDB = async () => {
+    try {
+        await mongoose.connection.close();
+        console.log("MongoDB Disconnected!!!");
+    } catch (error) {
+        console.error("Error disconnecting from MongoDB", error);
+        process.exit(1);
+    }
+}
+
+export default disconnectDB;
--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,60 @@ dotenv.config({ path: "./.env" });
 
 const port = process.env.PORT || 3000;
 
+let server;
+
+// Handle uncaught exceptions FIRST
+process.on("uncaughtException", (err) => {
+    console.error("Uncaught Exception:", err);
+    process.exit(1);
+});
+
+
 connectDB()
     .then(() => {
-        app.listen(port, () => {
-            console.log(`✔✅ App listening on Port http://localhost:${port}`);
+        server = app.listen(port, () => {
+            console.log(`✔ App listening on Port http://localhost:${port}`);
         });
     })
     .catch((err) => {
         console.error("MONGODB connection failed ", err);
     });
+
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+    console.error("Unhandled Rejection:", err);
+
+    if (server) {
+        server.close(async () => {
+            await disconnectDB();
+            process.exit(1);
+        });
+    } else {
+        process.exit(1);
+    }
+});
+
+// Graceful shutdown (Docker / hosting)
+process.on("SIGTERM", () => {
+    console.log("SIGTERM received. Shutting down...");
+
+    if (server) {
+        server.close(async () => {
+            await disconnectDB();
+            process.exit(0);
+        });
+    }
+});
+
+// Local shutdown (Ctrl + C)
+process.on("SIGINT", () => {
+    console.log("SIGINT received. Shutting down...");
+
+    if (server) {
+        server.close(async () => {
+            await disconnectDB();
+            process.exit(0);
+        });
+    }
+});
